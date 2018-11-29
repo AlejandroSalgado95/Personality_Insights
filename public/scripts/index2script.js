bf76fb8f8fc5531342ae0fc0808652f34b77cbce
YOUR NAME <YOUR@EMAIL.com>
2018-11-28 18:47:30 -0600

    ya esta el switch entre twitter y ensayo para que no salga el pop up

--- public/scripts/index2script.js
+++ public/scripts/index2script.js
@@ -1,3 +1,4 @@
+
 $('.switchAnalizar').on("click", function(event){
   event.preventDefault();
   console.log("entro");
@@ -10,28 +11,23 @@ $('.switchAnalizar').on("click", function(event){
   $('.selectDiv').addClass('hide');
 
   //Quitar las verificaciones de campos de todas las secciones
-  $(this).parent().attr('onsubmit', "");
+  $('#formAnalysis').attr('onsubmit', "");
   $("div#twitter input").prop('required', false);
 
   if($(this).attr('id') == 'ensayoSelect'){
     //mostrar la seccion de ensayo
     $('#ensayo').removeClass('hide');
 
-    // Poner el action correspondiente
-    //  $(this).parent().attr('action', '/PIServiceEssay');
-
     //Agregar las validaciones necesarias para el ensayo
-    $(this).parent().attr('onsubmit', "checkformEssay(event)");
+    $('#formAnalysis').attr('onsubmit', "checkformEssay(event)");
 
   }else{
     //mostrar la seccion de twitter
     $('#twitter').removeClass('hide');
 
-    // Poner el action correspondiente
-    //  $(this).parent().attr('action', '/PIServiceTwitter');
-
     //Agregar las validaciones necesarias para twitter
-    $("div#twitter input").prop('required',true);
+    $('#formAnalysis').attr('onsubmit', "checkformTwitter(event)");
+  //  $("div#twitter input").prop('required',true);
   }
 
 });
